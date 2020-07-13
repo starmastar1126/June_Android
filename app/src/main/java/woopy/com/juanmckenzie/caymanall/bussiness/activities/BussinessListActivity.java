22c3f31d62325921e33b07b1a7eb0a129208ac47
Silas Jo <52027648+starmaster1126@users.noreply.github.com>
2020-07-13 00:01:42 -0400

    version

--- app/src/main/java/woopy/com/juanmckenzie/caymanall/bussiness/activities/BussinessListActivity.java
+++ app/src/main/java/woopy/com/juanmckenzie/caymanall/bussiness/activities/BussinessListActivity.java
@@ -9,6 +9,6 @@ public class BussinessListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_bussiness_list);
+        setContentView(R.layout.activity_bussiness_list);
     }
 }
